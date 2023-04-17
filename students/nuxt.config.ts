78385776c082bc6c6677a9392b11004b2bff8c3c
GaryCraft <garycraft@our-space.xyz>
2023-04-16 21:31:11 -0600

    Add Auth-Handling to Students

--- students/nuxt.config.ts
+++ students/nuxt.config.ts
@@ -1,14 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	modules: [
-		'@vite-pwa/nuxt'
+		'@vite-pwa/nuxt',
+		'@sidebase/nuxt-auth',
 	],
 	pwa: {
 
 	},
 	runtimeConfig: {
 		public: {
-
+			oauthUrl: "https://oauth2.our-space.xyz",
 		},
 		location: {
 			center: {
