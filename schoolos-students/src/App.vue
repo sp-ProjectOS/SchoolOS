fd36a1f7b67f73f0dee380e0f2a10d4a4ed85416
GaryCraft <garycraft@our-space.xyz>
2024-12-07 12:26:41 -0600

    new students module

--- schoolos-students/src/App.vue
+++ schoolos-students/src/App.vue
@@ -3,43 +3,25 @@ import { useI18n } from 'vue-i18n';
 
 import { Card } from '@/components/ui/card';
 
-import DarkMode from '@/components/DarkMode.vue';
-import LangSelect from '@/components/LangSelect.vue';
-
+import NavMenu from '@/components/NavMenu.vue';
 
 const { t } = useI18n({
 	messages: {
 		en: {
-			
+
 		},
 		es: {
-			
+
 		}
 	}
 });
 
-
-
 </script>
 
 <template>
-	<div class="h-full flex-1 flex-col space-y-8 p-8">
-		<div class="flex items-center justify-between space-y-2">
-			<div class="flex flex-row items-center justify-center space-x-4 w-full">
-				Title
-			</div>
-			<div class="flex flex-row items-center space-x-2">
-				<LangSelect />
-				<DarkMode />
-			</div>
-		</div>
-		<div class="flex flex-row justify-evenly">
-			Body
-		</div>
-		<div class="w-full flex flex-row-reverse">
-			Footer
-		</div>
-	</div>
+	<main class="h-screen w-screen">
+		<NavMenu />
+	</main>
 </template>
 
 <style>
---
+++ schoolos-students/src/components/NavMenu.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from '@/components/ui/collapsible'
+import { LucideAlignJustify } from 'lucide-vue-next'
+
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import LangSelect from '@/components/LangSelect.vue'
+import DarkMode from '@/components/DarkMode.vue'
+
+const isOpen = ref(false)
+
+
+const { t, locale } = useI18n({
+	useScope: 'global',
+	messages: {
+		es: {
+			home: 'Inicio',
+			org: "Organizador"
+		},
+		en: {
+			home: 'Home',
+			org: "Scheduling"
+		}
+	}
+})
+</script>
+
+<template>
+	<Collapsible v-model:open="isOpen" class="absolute top-0 left-0">
+		<div class="flex items-center justify-start">
+			<CollapsibleTrigger as-child>
+				<Button variant="ghost" size="sm" class="flex w-12 rounded-br-3xl border p-0 animate-in animate-out"
+					v-if="!isOpen">
+					<LucideAlignJustify class="h-6 w-6" />
+					<span class="sr-only">Toggle</span>
+				</Button>
+				<Button variant="ghost" size="sm"
+					class="flex sm:rounded-br-3xl border pl-3 justify-start sm:w-96 w-screen animate-in animate-out"
+					v-else>
+					<LucideAlignJustify class="h-6 w-6" />
+					<div class="mx-auto text-lg" style="font-family: Carter One;">
+						SchoolOS
+					</div>
+					<span class="sr-only">Toggle</span>
+				</Button>
+			</CollapsibleTrigger>
+		</div>
+
+		<CollapsibleContent class="c-animated bg-gradient-to-br from-teal-300 to-cyan-300 w-screen sm:w-[90%]"
+			:style="{ width: isOpen ? '' : '0' }">
+			<nav class="h-[90svh] justify-start flex flex-col p-2 c-nav">
+				<a href="#"
+					class="block p-2 text-primary hover:text-secondary font-bold text-center hover:bg-primary border-primary border-b">
+					{{ t('home') }}
+				</a>
+				<a href="#"
+					class="block p-2 text-primary hover:text-secondary font-bold text-center hover:bg-primary border-primary border-b">
+					{{ t('org') }}
+				</a>
+
+				<div class="mt-auto gap-2 flex">
+					<DarkMode />
+					<LangSelect />
+				</div>
+			</nav>
+		</CollapsibleContent>
+	</Collapsible>
+</template>
+<style scoped>
+.c-animated {
+	transition: width 0.3s ease-in-out;
+	transition-behavior: smooth;
+}
+
+.c-nav {
+	overflow-y: auto;
+	overflow-x: hidden;
+}
+</style>
